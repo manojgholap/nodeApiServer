3d01babb11c2eebf5db563b97463c27e7e4958cb
manojgholap <manojgholap17021998@gmail.com>
2022-06-17 16:56:24 +0530

    save

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
-const PORT = 3200
+const PORT = process.env.port
 
 app.get("/",(req,res)=>{
     res.send({status:true,message:"server is started on port",PORT})
 })
 
 app.listen(PORT,()=>{
-    console.log("server is started on port",PORT);
+    console.log(`server is started on port ${PORT}`);
 })
